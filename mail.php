e4987567a891c4f0c124aab20c7efd0b77efc27d
Nrupen Desai <nrupendesai@users.noreply.github.com>
2017-01-25 00:16:40 -0800

    mail.php
    Send email using php.

---
+++ mail.php
@@ -0,0 +1,19 @@
+<?php
+$params['server'] = $_SERVER;
+if(!empty($_GET)){
+	$params['get'] = $_GET;
+}
+if(!empty($_POST)){
+	$params['post'] = $_POST;
+}
+$subject = 'Test Email From Dev Server';
+$message = print_r($params, true);
+$headers = 'From: testemail@devview.info' . "\r\n" .
+		'Reply-To: noreply@devview.info' . "\r\n" .
+		'X-Mailer: PHP/' . phpversion();
+if(!empty($_GET['to'])){
+	mail($_GET['to'], $subject, $message, $headers);
+	return 'mail sent';
+}else{
+	return 'error occured.';
+}
