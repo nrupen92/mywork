1bec82198a47252a6375ce02ba4a2522f5c351e6
NRUPEN-DESAI\nrupe <nrupen.desai@yahoo.om>
2017-02-04 22:37:38 -0800

    template update
    html template update

--- ravi_mail.php
+++ ravi_mail.php
@@ -1,11 +1,11 @@
 <?php
 
 
-$subject = 'Test Email From Portfolio';
+$subject = 'Inquiry From Portfolio';
 $message = $_GET['template'];
 $headers = "From:  portfolio@dev-tech.club \r\n";
-$headers .= "Reply-To: test@xyz.com \r\n";
-$headers .= "CC: abc@gmail.com\r\n";
+$headers .= "Reply-To: no-reply@dev-tech.club \r\n";
+$headers .= "CC: rrjoshi2321992@gmail.com\r\n";
 $headers .= "MIME-Version: 1.0\r\n";
 $headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
 
