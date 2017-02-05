bf25127aaeef2ebbd29c44802445ca3d3fcae2bb
NRUPEN-DESAI\nrupe <nrupen.desai@yahoo.om>
2017-02-04 22:27:40 -0800

    html template
    html mail template for ravi to send to his email

--- ravi_mail.php
+++ ravi_mail.php
@@ -2,7 +2,22 @@
 
 
 $subject = 'Test Email From Portfolio';
-$message = print_r($_POST, true);
-$headers = 'From: portfolio@dev-tech.club' . "\r\n" .
-			'X-Mailer: PHP/' . phpversion()."Content-Type: text/html; charset=ISO-8859-1\r\n";
+$message = "<html>
+<body>
+<div>
+	<div><label>Name: </label>  dsadjk</div>
+	<div><lable>Email: </lable> sdsf@dsad.com</div>
+	<div><label>Subject: </label>    dsfsd</div>
+    <div><label>Message: </label>    fdsfsdfds</div>
+</div>
+</body>
+</html>";
+$headers = "From:  portfolio@dev-tech.club \r\n";
+$headers .= "Reply-To: test@xyz.com \r\n";
+$headers .= "CC: abc@gmail.com\r\n";
+$headers .= "MIME-Version: 1.0\r\n";
+$headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
+
+
+
 mail('rrjoshi92@gmail.com', $subject, $message, $headers);
